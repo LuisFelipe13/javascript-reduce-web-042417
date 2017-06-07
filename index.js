91082be9211f87b08fefcf7604008057c8528c9d
Luis del Cueto <luisdelcueto@live.com>
2017-06-07 15:21:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 const app = "I don't do much."
+
+const products = [
+  { name: 'Head & Shoulders Shampoo', price: 4.99, discount: .6 },
+  { name: 'Twinkies', price: 7.99, discount: .45 },
+  { name: 'Oreos', price: 6.49, discount: .8 },
+  { name: 'Jasmine-scented bath pearls', price: 13.99, discount: .7 },
+];
+
+
+function getTotalAmountForProducts(products) {
+  let totalPrice = 0;
+
+  products.forEach(product => {
+    if (product.discount >= .5) {
+      totalPrice += product.price;
+    }
+  });
+
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products));
+
+
+
+//
+
+function getTotalAmountForProducts(products, callback) {
+  let totalPrice = 0;
+
+  products.forEach(product => {
+    totalPrice = callback(totalPrice, product);
+  });
+
+  return totalPrice;
+}
+
+function callback1(totalPrice, product) {
+  if (product.discount >= .5) {
+    return totalPrice + product.price;
+  }
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products, callback)); // prints 25.
+
+
+function callback2(totalPrice, product) {
+  if (product.price < 7) {
+    return totalPrice + product.price;
+  }
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products, callback)); // prints 11.5
